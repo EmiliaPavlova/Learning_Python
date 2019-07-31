e09e42606168bf1b85c817de2f88e3e1e2c080e6
Emilia Pavlova <epavl@sf500.softservecom.com>
2019-07-31 19:24:38 +0300

    Finish the last task from Loops

--- 05.1.Loops/12.EqualPairs.py
+++ 05.1.Loops/12.EqualPairs.py
@@ -1,7 +1,7 @@
 n = int(input())
 previous_value = None
 diff = 0
-max_diff = None
+max_diff = 0
 is_same = True
 
 
@@ -12,28 +12,16 @@ def get_max_number(num1, num2):
         return num2
 
 
-# diff and maxdiff not working!
-# https://python-book.softuni.bg/chapter-05-loops.html
-# https://judge.softuni.bg/Contests/Practice/Index/1053#11
-
 for i in range(0, n):
     first_number = int(input())
     second_number = int(input())
     pair_sum = first_number + second_number
+
     if previous_value:
         if previous_value != pair_sum:
             is_same = False
             diff = abs(pair_sum - previous_value)
-            print('diff: ', diff)
-            print('pair_sum', pair_sum)
-            print('previous_sum', previous_value)
-            if diff > max_diff:
-                max_diff = diff
-        # print('diff: ', diff)
-        # print('max_diff: ', max_diff)
-    else:
-        previous_value = pair_sum
-        max_diff = diff
+            max_diff = get_max_number(max_diff, diff)
+    previous_value = pair_sum
 
-print(max_diff)
-print(f'Yes, value={pair_sum}' if is_same else f'No, maxdiff=')
+print(f'Yes, value={pair_sum}' if is_same else f'No, maxdiff={max_diff}')
